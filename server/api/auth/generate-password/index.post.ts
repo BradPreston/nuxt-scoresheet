ab96a57b4da35bca10a93fd8de61bba03a29cc0d
Brad Preston <bap5393@gmail.com>
2026-08-02 08:44:58 -0400

    added route to generate a password

---
+++ server/api/auth/generate-password/index.post.ts
@@ -0,0 +1,22 @@
+import bcrypt from "bcrypt";
+import { z } from "zod";
+
+const passwordSchema = z.object({
+  password: z.string("Password must be included").min(8, "Password must be at least 8 characters long"),
+});
+
+export default defineEventHandler(async (event) => {
+  const body = await readValidatedBody(event, body => passwordSchema.safeParse(body));
+
+  if (!body.success) {
+    throw createError({
+      status: 400,
+      statusMessage: "Password error",
+      message: z.prettifyError(body.error),
+    });
+  }
+
+  const hashed = await bcrypt.hash(body.data.password, 10);
+
+  return { hashed };
+});
